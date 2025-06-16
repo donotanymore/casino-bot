9061c86a9742b490cd0a64d083a433ac28c03d70
donotanymore <donotanymore@outlook.com>
2025-06-17 00:20:11 +0700

    commit

--- bot.ps1
+++ bot.ps1
@@ -1,7 +1,13 @@
 # Start the VS Code tunnel in the background
 Start-Process "c:\Users\Admin\AppData\Local\Programs\Microsoft VS Code\bin\code-tunnel.exe" `
-    -ArgumentList 'tunnel --name pigletaccess' -NoNewWindow
-Start-Sleep -Seconds 5
+    -ArgumentList 'tunnel --accept-server-license-terms --log info --name pigletaccess --parent-process-id $PID --no-sleep' -NoNewWindow
+# Wait for the tunnel to start
+# Ensure the tunnel is running before proceeding
+do {
+    Start-Sleep -Seconds 1
+    $tunnelRunning = Get-Process -Name "code-tunnel" -ErrorAction SilentlyContinue
+} until ($tunnelRunning)
+write-host "VS Code tunnel is running"
 # Activate the virtual environment
 venv\Scripts\Activate.ps1
 # Install the required packages
